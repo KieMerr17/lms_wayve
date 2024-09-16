d207dc50dc9236c0559329acd1155f31832dfc5f
Kieran Merrett <116359190+KieMerr17@users.noreply.github.com>
2024-09-16 21:48:37 +0100

    add course model to project'

--- lms/admin.py
+++ lms/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
- 
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date', 'is_active')
+    search_fields = ('title', 'description', 'instructor__username')
+    list_filter = ('start_date', 'end_date', 'is_active')
+    
--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField() 
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['start_date']
