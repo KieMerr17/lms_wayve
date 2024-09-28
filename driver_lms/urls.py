54128af9e062d6c0e84fd51c6d90cd660456dda0
Kieran Merrett <116359190+KieMerr17@users.noreply.github.com>
2024-09-28 20:24:40 +0100

    adjust functionality on training records to match qa report

--- driver_lms/urls.py
+++ driver_lms/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('dashboard/', dashboard_view, name='dashboard'),
     path('signup/', signup_view, name='signup'),
     path('admin/', admin.site.urls),
-] 
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
